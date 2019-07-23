a3c8fe986eb9751aaea35b4a74a6bd3ee7290666
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-23 14:54:15 -0500

    service implement mejorando

--- ServiceSecond/src/main/java/com/everis/serviceimplement/MSStudentsServiceImplement.java
+++ ServiceSecond/src/main/java/com/everis/serviceimplement/MSStudentsServiceImplement.java
@@ -2,7 +2,9 @@ package com.everis.serviceimplement;
 
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +19,22 @@ public class MSStudentsServiceImplement implements MSStudentsService {
 
 	@Autowired
 	private RestTemplate studentRest;
+	
 	@Override
 	public List<MSStudents> finAll() {
 		List<Students> students = Arrays.asList(studentRest.getForObject("http://localhost:8010/Get", Students[].class));
 		
-		return students.stream().map(s -> new MSStudents(s,Date).collect(Collectors.toList());
+		
+		Date date = null;
+		return students.stream().map(s -> new MSStudents(s,date)).collect(Collectors.toList());
 	}
 
 	@Override
 	public MSStudents finById(Integer id, Date date) {
-		return null;
+		Map<String,String> pathvariables = new HashMap<String,String>();
+		pathvariables.put("id", id.toString());
+		Students students = studentRest.getForObject("http://localhost:8010/Get/{id}",Students.class );
+		return new MSStudents(students, date);
 	}
 
 }
