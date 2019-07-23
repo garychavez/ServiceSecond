834854a6bd09e43fd9673d81a66f006085d01d10
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-23 13:59:50 -0500

    service

---
+++ ServiceSecond/src/main/java/com/everis/service/MSStudents.java
@@ -0,0 +1,10 @@
+package com.everis.service;
+
+import java.util.Date;
+import java.util.List;
+
+public interface MSStudents {
+public List<MSStudents> finAll();
+public MSStudents finById(Integer id, Date date);
+
+}
