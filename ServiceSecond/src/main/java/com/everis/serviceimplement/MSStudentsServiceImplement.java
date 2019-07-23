de0e60d9a90e88b410fa6b6e8c8be448a5eae690
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-23 14:53:25 -0500

    serviceImplement

--- ServiceSecond/src/main/java/com/everis/serviceimplement/MSStudentsServiceImplement.java
+++ ServiceSecond/src/main/java/com/everis/serviceimplement/MSStudentsServiceImplement.java
@@ -1,18 +1,27 @@
 package com.everis.serviceimplement;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.everis.model.MSStudents;
+import com.everis.model.Students;
 import com.everis.service.MSStudentsService;
 @Service
 public class MSStudentsServiceImplement implements MSStudentsService {
 
+	@Autowired
+	private RestTemplate studentRest;
 	@Override
 	public List<MSStudents> finAll() {
-		return null;
+		List<Students> students = Arrays.asList(studentRest.getForObject("http://localhost:8010/Get", Students[].class));
+		
+		return students.stream().map(s -> new MSStudents(s,Date).collect(Collectors.toList());
 	}
 
 	@Override
