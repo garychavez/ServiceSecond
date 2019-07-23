461a7d5d92436173bf56720276fabf3e9e412a75
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-23 15:04:51 -0500

    controlador

---
+++ ServiceSecond/src/main/java/com/everis/controller/MSStudentsController.java
@@ -0,0 +1,30 @@
+package com.everis.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.everis.model.MSStudents;
+import com.everis.service.MSStudentsService;
+
+@RestController
+public class MSStudentsController {
+
+	@Autowired
+	private MSStudentsService msStudentsService;
+	
+	@GetMapping("/listar")
+	public List<MSStudents> listar(){
+		return msStudentsService.finAll();
+	}
+	
+	@GetMapping("Get/{id}/Date/{date}")
+	public MSStudents detalle(@PathVariable Integer id,@PathVariable Date date) {
+		return msStudentsService.finById(id, date);
+		
+	}
+}
