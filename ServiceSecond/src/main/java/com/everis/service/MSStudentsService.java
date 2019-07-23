c5995817e6847dfbda0d3fcb7a1965ebed8fd73f
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-23 14:01:55 -0500

    rename service paquete

--- ServiceSecond/src/main/java/com/everis/service/MSStudentsService.java
+++ ServiceSecond/src/main/java/com/everis/service/MSStudentsService.java
@@ -3,7 +3,9 @@ package com.everis.service;
 import java.util.Date;
 import java.util.List;
 
-public interface MSStudents {
+import com.everis.model.MSStudents;
+
+public interface MSStudentsService{
 public List<MSStudents> finAll();
 public MSStudents finById(Integer id, Date date);
 
