626f324cecf9e7f8e9b784d2db2f823cb2ecb78d
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-23 14:08:32 -0500

    appconfig

--- ServiceSecond/src/main/java/com/everis/AppConfig.java
+++ ServiceSecond/src/main/java/com/everis/AppConfig.java
@@ -1,5 +1,14 @@
 package com.everis;
 
-public class AppConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
+@Configuration
+public class AppConfig {
+@Bean(name="studentRest")
+public RestTemplate resgistrarRestTemplate() {
+	return new RestTemplate();
+	
+}
 }
--- ServiceSecond/src/main/java/com/everis/serviceimplement/MSStudentsServiceImplement.java
+++ ServiceSecond/src/main/java/com/everis/serviceimplement/MSStudentsServiceImplement.java
@@ -3,20 +3,20 @@ package com.everis.serviceimplement;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.everis.model.MSStudents;
 import com.everis.service.MSStudentsService;
-
+@Service
 public class MSStudentsServiceImplement implements MSStudentsService {
 
 	@Override
 	public List<MSStudents> finAll() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public MSStudents finById(Integer id, Date date) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
