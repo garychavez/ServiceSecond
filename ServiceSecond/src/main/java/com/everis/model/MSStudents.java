e84edd13d433fd25c3bbe922a8a0bc38ef0d5a48
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-23 12:00:01 -0500

    application properties

--- ServiceSecond/src/main/java/com/everis/model/MSStudents.java
+++ ServiceSecond/src/main/java/com/everis/model/MSStudents.java
@@ -1,5 +1,36 @@
 package com.everis.model;
 
+import java.util.Date;
+
 public class MSStudents {
+	private Students students;
+	private Date date;
+
+// constructor
+
+	public MSStudents() {
+	}
+
+	public MSStudents(Students students, Date date) {
+		this.students = students;
+		this.date = date;
+	}
+
+//	getters and setters
+	public Students getStudents() {
+		return students;
+	}
+
+	public void setStudents(Students students) {
+		this.students = students;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
 }
